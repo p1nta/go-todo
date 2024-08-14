46cc64a0657e4eef4e0aab22e0247782e53682c8
p1nta <crispworm@gmail.com>
2024-08-14 19:50:29 +0300

    chore: try to deploy vercel

--- api/main.go
+++ api/main.go
@@ -23,7 +23,7 @@ type Todo struct {
 var collection *mongo.Collection
 
 func main() {
-	err := godotenv.Load(".env")
+	err := godotenv.Load("../.env")
 
 	if err != nil {
 		log.Fatal("Error to loading .env file")
